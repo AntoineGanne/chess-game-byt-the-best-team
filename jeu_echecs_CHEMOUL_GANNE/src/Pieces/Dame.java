d829756c4269119ee91505ca056d0e802b7d79b0
antoine ganne <antoine.ganne@etu.univ-lyon1.fr>
2018-01-09 18:00:13 +0100

    Refactorisation + amelioration estEnEchec() + correctifs mineurs
    c'est mal d'ecrire une tonne de code copié colllé D: :anguished:
    la refactorisation, c'est le bien :p

--- jeu_echecs_CHEMOUL_GANNE/src/Pieces/Dame.java
+++ jeu_echecs_CHEMOUL_GANNE/src/Pieces/Dame.java
@@ -14,6 +14,32 @@ public class Dame extends Piece {
 
         LinkedList<Case> casesPossibles = new LinkedList<Case>();
 
+
+        //on parcoure toutes les dirrections autours de (x,y)
+        for(int xOffset=-1;xOffset<=1;xOffset++)
+        {
+            for(int yOffset=-1;yOffset<=1;yOffset++)
+            {
+                if(!(xOffset==0 && yOffset==0)){
+                    boolean pieceNonAtteinte = true;  //pour sortir de la boucle après avoir atteint une pièce
+                    int xTemp = x+xOffset;
+                    int yTemp = y+yOffset;
+                    while(xTemp>=0 && xTemp < 8 && yTemp>=0 && yTemp<8 && pieceNonAtteinte){
+                        //si la case est vide ou qu'elle contient une piece de la couleur opposée
+                        boolean b_estVide =tabJeu[xTemp][yTemp].estVide();
+                        if(b_estVide || (this.isEstBlanc() ? !tabJeu[xTemp][yTemp].getPiece().isEstBlanc() : tabJeu[xTemp][yTemp].getPiece().isEstBlanc())){
+                            casesPossibles.add(tabJeu[xTemp][yTemp]);
+                        }
+                        if(!b_estVide) pieceNonAtteinte=false;
+                        xTemp+=xOffset;
+                        yTemp+=yOffset;
+                    }
+                }
+            }
+        }
+
+        return casesPossibles;
+        /*
         //Verticalement
         int i =1;
         while(x+i<=7){
@@ -98,7 +124,9 @@ public class Dame extends Piece {
                 break; //on affiche pas les cases qui sont derrière une case occupée (le fou ne peut pas sauter par dessus une case)
             i++;
         }
+        */
 
-        return casesPossibles;
+
+
     }
 }
